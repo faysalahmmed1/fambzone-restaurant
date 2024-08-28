17caf22e93fa9d55681824365adac41c66d5ee46
faysalahmmed1 <faysalahmmedcse@gmail.com>
2024-08-29 00:40:22 +0600

    stripe for react and create card element

--- src/Pages/DashBoard/Payment/Payment.jsx
+++ src/Pages/DashBoard/Payment/Payment.jsx
@@ -1,10 +1,19 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
-
-
+import { Elements } from "@stripe/react-stripe-js";
+//TO DO: 
+const stripePromise = loadStripe('')
 const Payment = () => {
     return (
-        <div className="text-center">
-            <SectionTitle subHeding='please pay to eat' heading='Payment'></SectionTitle>
+        <div>
+            <div className="text-center">
+                <SectionTitle subHeding='please pay to eat' heading='Payment'>
+
+                </SectionTitle>
+            </div>
+            <div>
+                <Elements stripe={stripePromise}></Elements>
+            </div>
         </div>
     );
 };
